b9b4e8fc41284e4608b992b35be39d40097b9f54
Asrahal <maxence.ouafik@gmail.com>
2020-10-28 23:03:15 +0100

    Update

--- scripts/read_data.R
+++ scripts/read_data.R
@@ -6,4 +6,8 @@ Survey_Data <- read_delim("data/Survey_Data.csv",
                           col_types = cols(X1 = col_skip(), X7 = col_skip()), 
                           locale = locale(encoding = "WINDOWS-1252"), 
                           trim_ws = TRUE, skip = 1) %>%
-  rename("Assignation" = "X2", "Confort_AFAB" = "X3", "Inconfort_AFAB" = "X4", "Confort_AMAB" = "X5", "Inconfort_AMAB" = "X6")
+  rename("Assignation" = "X2", 
+         "Confort_AFAB" = "X3", 
+         "Inconfort_AFAB" = "X4", 
+         "Confort_AMAB" = "X5", 
+         "Inconfort_AMAB" = "X6")
